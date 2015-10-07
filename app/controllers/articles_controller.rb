a86ac426f04a7b2b481f9dcaa4edbad176dfcd2f
John Gleeson <johnnygleeson@gmail.com>
2015-10-07 14:35:30 +0100

    new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,17 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
 
 	def index
+		if params[:category].blank?
+			@articles = Article.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@articles = Article.where(category_id: @category_id).order("created_at DESC")
+		end
+	end
 
+	def show
 	end
 
 	def new
@@ -9,7 +19,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@artcle = Article.new(article_params)
+		@article = current_user.articles.build(article_params)
 		if @article.save
 			redirect_to @article
 		else
@@ -17,9 +27,29 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to root_path
+	end
+
 	private
 
+	def find_article
+		@article = Article.find(params[:id])
+	end
+
 	def article_params
-		params.require(:article).permit(:title, :content)
+		params.require(:article).permit(:title, :content, :category_id)
 	end
 end
