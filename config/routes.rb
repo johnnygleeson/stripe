421bc8541a0fb443a457403f07c78afcefa45eaa
John Gleeson <johnnygleeson@gmail.com>
2015-10-08 09:35:56 +0100

    fixed navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
-  root 'articles#index'
+  root   'articles#index'
+  get '/users/sign_out' => 'devise/sessions#destroy'
+
 end
